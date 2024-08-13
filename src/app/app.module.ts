7e6bc605447fb717168dca2aea5aec60a5906e29
Pilo28 <pdefelitto@gmail.com>
2024-08-13 11:55:59 +0200

    added interceptors errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorHandlingInterceptor } from './core/interceptors/error-handling-interceptor.interceptor';
+import { loggingInterceptor } from './core/interceptors/logging-interceptor.interceptor';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,13 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(
+      withInterceptors([
+        errorHandlingInterceptor, loggingInterceptor
+      ])
+    ),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
