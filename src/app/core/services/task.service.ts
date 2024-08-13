1e2547f108131ed11b7b295b5971e7c1e86150b8
Pilo28 <pdefelitto@gmail.com>
2024-08-13 12:20:08 +0200

    added service

--- src/app/core/services/task.service.ts
+++ src/app/core/services/task.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  private tasksCollection: any;
+
+  constructor(private firestore: AngularFirestore) {
+    this.tasksCollection = this.firestore.collection('tasks');
+  }
+
+  getTasks(): Observable<any[]> {
+    return this.tasksCollection.valueChanges({ idField: 'id' });
+  }
+
+  addTask(task: any): Promise<any> {
+    return this.tasksCollection.add(task);
+  }
+
+  updateTask(id: string, task: any): Promise<void> {
+    return this.tasksCollection.doc(id).update(task);
+  }
+
+  deleteTask(id: string): Promise<void> {
+    return this.tasksCollection.doc(id).delete();
+  }
 }
