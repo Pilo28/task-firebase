dd739e6b63200da42380a04909c95e407f4422cf
Pilo28 <pdefelitto@gmail.com>
2024-08-13 12:16:41 +0200

    added firebase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyA7Ozu7e3xUMyzN46SsZfocSea5hs54QFk",
+        authDomain: "task-firebase-fdd0b.firebaseapp.com",
+        projectId: "task-firebase-fdd0b",
+        storageBucket: "task-firebase-fdd0b.appspot.com",
+        messagingSenderId: "87922201259",
+        appId: "1:87922201259:web:22862b79a2a82e3df39dee",
+        measurementId: "G-KT8DTL90Y5"
+    }
+  };
+  
