87b09e412a4a7f0d9cabd2a88fd848ed405a3dec
Pilo28 <pdefelitto@gmail.com>
2024-08-13 11:33:55 +0200

    added base

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -3,12 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-  {path: 'dashboard', loadChildren: () => import('./features/features.module').then(m => m.FeaturesModule)},
-  {path: '**', redirectTo: 'dashboard'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class FeaturesRoutingModule { }
